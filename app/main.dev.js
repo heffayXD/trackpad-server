d64d50b10f0668aa51bf362600136e1b789e46c5
Jeff <heffayXD@gmail.com>
2020-01-22 14:20:16 -0700

    Removed something I shouldn't have. Oops.

--- app/main.dev.js
+++ app/main.dev.js
@@ -1,3 +1,4 @@
+import 'regenerator-runtime'
 import path from 'path'
 import { app, BrowserWindow, ipcMain } from 'electron'
 import { onStartServer, onStopServer, onCheckServer } from './ipc/server'
